930e5f77740b69fea84b4ec34e0736e8b63cfd32
cairuyuan <cairuyuan@hotmail.com>
2014-07-08 14:43:38 +0800

    change files

---
+++ graph/code_2.h
@@ -0,0 +1,53 @@
+#ifndef __CODE_2_H__
+#define __CODE_2_H__
+
+
+#include "stdafx.h"
+#include "head.h"
+#include "Tree.h"
+#include "LinkNode.h"
+#include "code_1.h"
+
+using namespace std;
+struct Interval
+{
+public:
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
+bool wordBreak(string s, unordered_set<string> &dic);
+int decode(string s);
+int lengthOfLastWord(const char *s);
+bool isMatch(const char *s, const char *p);
+string getPermutation(int n, int k);
+ListNode *deleteDuplicates(ListNode * head);
+vector<int> grayCode(int n);
+TreeNode *sortedListToBST(ListNode *head);
+void solve(vector<vector<char>> &board);
+
+
+int searchInsert_(int A[], int n, int target);
+bool searchMatrix(vector<vector<int> > &matrix, int target);
+string longestCommonPrefix(vector<string> &strs);
+
+vector<Interval> insert(vector<Interval> &intervals, Interval newInterval);
+vector<Interval> merge(vector<Interval> &intervals);//----------------------------------------74
+int longestConsecutive(vector<int> &num);
+
+int longestValidParentheses(string s);
+int trap(int A[], int n);
+vector<string> letterCombinations(string digits);
+double findMedianSortedArrays(int A[], int m, int B[], int n);
+int maxProfit(vector<int> &prices);
+int maxProfit2(vector<int> &prices);
+int maxProfit3(vector<int> &prices);
+
+void sortColors(int A[], int n);
+int canCompleteCircuit(vector<int> &gas, vector<int> &cost);
+vector<vector<int> > combine(int n, int k);
+int canCompleteCircuit(vector<int> &gas, vector<int> &cost);
+
+#endif
---
+++ graph/code_3.cpp
@@ -0,0 +1,11 @@
+
+#include "stdafx.h"
+#include "head.h"
+#include "Tree.h"
+#include "LinkNode.h"
+#include "code_2.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
